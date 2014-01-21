c8a64dea8da33b1e7423724bee2410b5efcff2e1
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2014-01-21 17:50:51 -0500

    experimenting with toxin features

---
+++ Jan21.py
@@ -0,0 +1,76 @@
+import numpy as np 
+import data 
+import amino_acid
+
+import sklearn
+import sklearn.cross_validation 
+import sklearn.ensemble
+import sklearn.svm
+
+
+print "Loading data and transforming to toxin features"
+X,Y = data.load_toxin_features(substring_length=2)
+
+def run_classifiers(X,Y):
+  print "Data shape", X.shape
+  for c in [0.01, 0.1, 1, 10]:
+    svm = sklearn.svm.LinearSVC(C=c)
+    print "SVM C =", c
+    print np.mean(sklearn.cross_validation.cross_val_score(svm, X, Y, cv = 10))
+ 
+  n_classifiers = 100
+  rf = sklearn.ensemble.RandomForestClassifier(n_classifiers)
+  print "Random Forest"
+  print np.mean(sklearn.cross_validation.cross_val_score(rf, X, Y, cv = 10))
+
+"""
+Toxin features alone seem to do terribly
+"""
+#run_classifiers(X,Y)
+
+"""
+Try reducing the dimensionality 
+"""
+import sklearn.decomposition
+
+
+print 
+print "PCA n = 25"
+pca = sklearn.decomposition.PCA(n_components=25)
+X_small = pca.fit_transform(X)
+run_classifiers(X_small,Y)
+
+
+print 
+print "PCA n = 50"
+pca = sklearn.decomposition.PCA(n_components=50)
+X_small = pca.fit_transform(X)
+run_classifiers(X_small,Y)
+
+
+print 
+print "PCA n = 100"
+pca = sklearn.decomposition.PCA(n_components=100)
+X_small = pca.fit_transform(X)
+run_classifiers(X_small,Y)
+
+print 
+print "Dictionary n = 25"
+d = sklearn.decomposition.MiniBatchDictionaryLearning(n_components=25)
+X_small = d.fit_transform(X)
+run_classifiers(X_smaller,Y)
+
+
+print 
+print "Dictionary n = 50"
+pca = sklearn.decomposition.MiniBatchDictionaryLearning(n_components=50)
+X_small = d.fit_transform(X)
+run_classifiers(X_small,Y)
+
+
+print 
+print "Dictionary n = 100"
+d = sklearn.decomposition.MiniBatchDictionaryLearning(n_components=100)
+X_small = d.fit_transform(X)
+run_classifiers(X_small,Y)
+
--- data.py
+++ data.py
@@ -12,7 +12,9 @@ def load_9mers(filename):
   result = [peptide_to_indices(line) for line in lines]
   return np.array(result)
 
-def load_dataset(imm_file = 'IMMA2_imm.txt', non_file='IMMA2_non.txt'):
+IMM_FILE = 'IMMA2_imm.txt'
+NON_FILE = 'IMMA2_non.txt'
+def load_dataset(imm_file = IMM_FILE, non_file= NON_FILE):
   imm = load_9mers(imm_file) 
   non = load_9mers(non_file)
   X = np.vstack([imm, non])
@@ -44,3 +46,16 @@ def transform(X, fns, positions = None, mean = False, pairwise_ratios = False):
     X2.append(row)
   X2 = np.array(X2)
   return X2
+
+import toxin 
+def load_toxin_features(imm_file = IMM_FILE, non_file = NON_FILE, substring_length=3):
+  imm = load_lines(imm_file)
+  imm_toxin_features = toxin.toxin_features(imm, length=substring_length)
+  non = load_lines(non_file)
+  non_toxin_features = toxin.toxin_features(non, length=substring_length)
+  X = np.vstack([imm_toxin_features, non_toxin_features])
+  Y = np.ones(len(X), dtype='bool')
+  Y[len(imm):] = 0
+  return X, Y
+
+  
