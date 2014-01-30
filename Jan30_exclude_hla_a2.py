4de512188aff38d3bb187f22270ab3f38ddc0b32
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2014-01-30 18:33:51 -0500

    fixed small typo

--- Jan30_exclude_hla_a2.py
+++ Jan30_exclude_hla_a2.py
@@ -23,7 +23,9 @@ X_all, Y_all = iedb.load_dataset(
                  only_hla_a2 = False)
 eval_dataset.eval_cv(X_all, Y_all)
 
+
 # also try filtering out the contradictory entries
+print
 print "All species filtered"
 X_all_filter, Y_all_filter = iedb.load_dataset(
                  filter_noisy_labels = True,
@@ -49,7 +51,7 @@ X_human_filter, Y_human_filter = iedb.load_dataset(
                  hla_type1 = False,
                  exclude_hla_a2 = False, 
                  only_hla_a2 = False)
-eval_dataset.eval_cv(X_human, Y_human)
+eval_dataset.eval_cv(X_human_filter, Y_human_filter)
 
 print "Human MHC1"
 X_human_mhc1, Y_human_mhc1 = iedb.load_dataset(
@@ -87,8 +89,6 @@ X_no_hla_a2_filter, Y_no_hla_a2_filter = iedb.load_dataset(
                  hla_type1 = True,
                  exclude_hla_a2 = True, 
                  only_hla_a2 = False)
-                 
-
 eval_dataset.eval_cv(X_no_hla_a2_filter, Y_no_hla_a2_filter)
 
 
@@ -99,5 +99,4 @@ X_hla_a2, Y_hla_a2 = iedb.load_dataset(
                  hla_type1 = True,
                  exclude_hla_a2 = False, 
                  only_hla_a2 = True)
-
 eval_dataset.eval_split(X_no_hla_a2, Y_no_hla_a2, X_hla_a2, Y_hla_a2)
